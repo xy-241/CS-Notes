7a12e40e291f6ae468808bc8c582ff321c8a63d3
Jacky Zhao <j.zhao2k19@gmail.com>
2023-12-17 12:27:19 -0800

    site update
    clipboard fix & upstream updates

--- quartz.config.ts
+++ quartz.config.ts
@@ -50,7 +50,6 @@ const config: QuartzConfig = {
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "filesystem"], // you can add 'git' here for last modified from Git but this makes the build slower
       }),
-      Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
