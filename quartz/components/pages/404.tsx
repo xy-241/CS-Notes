6996c4cd2a23e0900f439181c9d51795fd6ad2b5
XinYang YU <me@yxy.ninja>
2024-01-13 00:36:14 +0800

    site update

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -3,8 +3,11 @@ import { QuartzComponentConstructor } from "../types"
 function NotFound() {
   return (
     <article class="popover-hint">
-      <h1>404</h1>
-      <p>Either this page is private or doesn't exist.</p>
+      <h1>Oops</h1>
+      <p>I am still working on this note. Tell me <a src={"https://github.com/xy-241/CS-Notes/issues"}>here</a> if you want to read it urgently!</p>
+
+      <p>Meanwhile, here is a cute cat to express my apologies :)</p>
+      <img src={"https://cataas.com/cat/cute/says/Github Star Pls?fontColor=white&fontSize=50"} alt={"cuteCat"} />
     </article>
   )
 }
