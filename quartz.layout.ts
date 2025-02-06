fd519e2538112cb3f1d576525688d67959544f68
Xinyang YU <47915643+xy-241@users.noreply.github.com>
2025-02-06 23:30:03 +0800

    disbale mobile explorer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,7 +29,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Darkmode(),
     Component.DesktopOnly(Component.RecentNotes({ linkToMore: "tags/" , limit: 3, showTags: false })),
     Component.DesktopOnly(Component.Explorer()),
-    Component.Explorer(),
+    //Component.MobileOnly(Component.Explorer()),
   ],
   right: [
     // Component.MobileOnly(Component.Explorer()),
